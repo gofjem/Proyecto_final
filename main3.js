5d7cc6e9179a0393f79bfb1d2f286afa8557aa82
jesusMGonzalez <caccay.gestion@gmail.com>
2024-05-09 21:36:34 -0400

    mejoras en carro de compra

--- main3.js
+++ main3.js
@@ -4,33 +4,36 @@ const mostrarCarro=document.getElementById("carrito-container")
 const cantidadcarro= document.getElementById("cantidadCarro")
 
 class Servicio{
-    // constructor(nombreServicio,descripcion,categoria,imagen,precio,cantidad){
-    //     this.nombreServicio=nombreServicio
-    //     this.descripcion=descripcion
-    //     this.categoria=categoria
-    //     this.imagen=imagen
-    //     this.precio=precio
-    //     this.cantidad=cantidad
-    // }
-    constructor(){
-      
+    constructor(id,nombreServicio,descripcion,categoria,imagen,precio,cantidad){
+        this.id=id
+        this.nombreServicio=nombreServicio
+        this.descripcion=descripcion
+        this.categoria=categoria
+        this.imagen=imagen
+        this.precio=precio
+        this.cantidad=cantidad
     }
 }
+// class carroDeCompra{
+//   constructor(){
+//     this.servicios=
+//   }
+// }
 
-// const servicios = [
-//     new Servicio ("Instalación de Griferia","Se repara cualquier averia que tenga en su cocina, baños, etc","plomeria","https://sodimac.scene7.com/is/image/SodimacCL/3686183_01?wid=800&hei=800&qlt=70",10000,1),
-//     new Servicio ("Reparación de tuberias","Se repara cualquier averia que tenga en su cocina, baños, etc","plomeria","https://www.cronoshare.com/blog/wp-content/uploads/2020/11/C%C3%B3mo-reparar-una-tuber%C3%ADa-de-cobre.jpg",20000,1),
-//     new Servicio ("Instalación de Sanitarios","Se repara cualquier averia que tenga en su cocina, baños, etc","plomeria","https://maestros.com.co/wp-content/uploads/2016/10/Instalaci%C3%B3n-de-sanitario-1.jpg",30000,1),
-//     new Servicio ("Instalación de Lamparas","Se repara cualquier averia que tenga en su cocina, baños, etc","electricidad","https://cdn.manomano.com/media/edison/9/3/c/e/93ce2685d4dd.jpg",15000,1),
-//     new Servicio ("Reparación de neveras","Se repara cualquier averia que tenga en su cocina, baños, etc","electricidad","https://www.serviciotecnicoenbarcelona.es/wp-content/uploads/2020/10/neveras-1.jpg",25000,1),
-//     new Servicio ("Reparación de hornos","Se repara cualquier averia que tenga en su cocina, baños, etc","electricidad","https://servicio-tecnico-en-lima.com/wp-content/uploads/2021/03/6-48-1024x576-1.jpg",35000,1),
-//     new Servicio ("Muebles a medida","Se repara cualquier averia que tenga en su cocina, baños, etc","carpinteria","https://images.hola.com/imagenes/decoracion/20211202200655/muebles-a-medida-cuando-y-como-elegirlos-il/1-26-833/muebles-a-medida-11a-a.jpg",55000,1),
-//     new Servicio ("Instalación de pisos de madera y/o flotante","Se repara cualquier averia que tenga en su cocina, baños, etc","carpinteria","https://i.ytimg.com/vi/YOQdo1evTnw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAHoND7dNuYA1aNNHPrV_EJZGV9bg",98000,1),
-//     new Servicio ("Armado de muebles y mesas","Se repara cualquier averia que tenga en su cocina, baños, etc","armado","https://sodimac.scene7.com/is/image/SodimacCL/6441009_01?wid=800&hei=800&qlt=70",35000,1),
-//     new Servicio ("Armado de comodas y otros","Se repara cualquier averia que tenga en su cocina, baños, etc","armado","https://static2.elcontainer.cl/4318-thickbox_default/armado-de-mueble-solo-retiro-en-tienda.jpg",25000,1)
-// ];
+const servicios = [
+    new Servicio (1,"Instalación de Griferia","Se repara cualquier averia que tenga en su cocina, baños, etc","plomeria","https://sodimac.scene7.com/is/image/SodimacCL/3686183_01?wid=800&hei=800&qlt=70",10000,1),
+    new Servicio (2,"Reparación de tuberias","Se repara cualquier averia que tenga en su cocina, baños, etc","plomeria","https://www.cronoshare.com/blog/wp-content/uploads/2020/11/C%C3%B3mo-reparar-una-tuber%C3%ADa-de-cobre.jpg",20000,1),
+    new Servicio (3,"Instalación de Sanitarios","Se repara cualquier averia que tenga en su cocina, baños, etc","plomeria","https://maestros.com.co/wp-content/uploads/2016/10/Instalaci%C3%B3n-de-sanitario-1.jpg",30000,1),
+    new Servicio (4,"Instalación de Lamparas","Se repara cualquier averia que tenga en su cocina, baños, etc","electricidad","https://cdn.manomano.com/media/edison/9/3/c/e/93ce2685d4dd.jpg",15000,1),
+    new Servicio (5,"Reparación de neveras","Se repara cualquier averia que tenga en su cocina, baños, etc","electricidad","https://www.serviciotecnicoenbarcelona.es/wp-content/uploads/2020/10/neveras-1.jpg",25000,1),
+    new Servicio (6,"Reparación de hornos","Se repara cualquier averia que tenga en su cocina, baños, etc","electricidad","https://servicio-tecnico-en-lima.com/wp-content/uploads/2021/03/6-48-1024x576-1.jpg",35000,1),
+    new Servicio (7,"Muebles a medida","Se repara cualquier averia que tenga en su cocina, baños, etc","carpinteria","https://images.hola.com/imagenes/decoracion/20211202200655/muebles-a-medida-cuando-y-como-elegirlos-il/1-26-833/muebles-a-medida-11a-a.jpg",55000,1),
+    new Servicio (8,"Instalación de pisos de madera y/o flotante","Se repara cualquier averia que tenga en su cocina, baños, etc","carpinteria","https://i.ytimg.com/vi/YOQdo1evTnw/hq720.jpg?sqp=-oaymwEhCK4FEIIDSFryq4qpAxMIARUAAAAAGAElAADIQj0AgKJD&rs=AOn4CLAHoND7dNuYA1aNNHPrV_EJZGV9bg",98000,1),
+    new Servicio (9,"Armado de muebles y mesas","Se repara cualquier averia que tenga en su cocina, baños, etc","armado","https://sodimac.scene7.com/is/image/SodimacCL/6441009_01?wid=800&hei=800&qlt=70",35000,1),
+    new Servicio (10,"Armado de comodas y otros","Se repara cualquier averia que tenga en su cocina, baños, etc","armado","https://static2.elcontainer.cl/4318-thickbox_default/armado-de-mueble-solo-retiro-en-tienda.jpg",25000,1)
+];
 
-let carroCompra= JSON.parse(localStorage.getItem("Carro"))||[]
+let carroCompra= JSON.parse(localStorage.getItem("Carro"))  || []
 
 const postearServicios = (servicio) =>{
   let contenido= document.createElement("div")
@@ -49,34 +52,34 @@ const postearServicios = (servicio) =>{
 
   contenido.append(agregar)
 
+  
+
   agregar.addEventListener("click",()=>{
 
   // Validar si un servicio esta repetido 
-    const servicioRepetido=carroCompra.some((repetido)=> repetido.nombre === servicio.nombreServicio)
-    console.log(servicioRepetido);
+    const servicioRepetido=carroCompra.some((repetido)=> repetido.id === servicio.id)
 
     if(servicioRepetido){
       carroCompra.map((serv)=>{
-        if(serv.nombre === servicio.nombre){
-          serv.cantidad++
-        }
+       if(serv.id === servicio.id){
+        serv.cantidad++
+       } 
       })
     }else{
       carroCompra.push({
+        id: servicio.id,
         img: servicio.imagen,
         nombre: servicio.nombreServicio,
         precio: servicio.precio,
         cantidad: servicio.cantidad
       })
     }
-    console.log(carroCompra);
     contadorCarro()
     guardarStorage()
    
   })
- 
+} 
   
-}
 
 servicios.forEach((servicio)=>{
   postearServicios(servicio)
@@ -109,7 +112,7 @@ fetch("./servicios.json")
 })
 .then(productos=>{
   productos.forEach(producto=>{
-
+    postearServicios(producto)
   })
 })
 
@@ -122,7 +125,7 @@ fetch("./servicios.json")
       const headerCarrito= document.createElement("div")
       headerCarrito.className="modal-contenido-carro"
       headerCarrito.innerHTML= `
-      <h2 class="header-carrito">Tu Carrito:</h2>
+      <h2 class="header-carrito">Tu Carro de compra:</h2>
       `
       mostrarCarro.append(headerCarrito)
      
@@ -180,10 +183,16 @@ fetch("./servicios.json")
       contadorCarro()
       desplegarCarro()
       Swal.fire({
-        title: `¡Su producto se ha eliminado correctamente!`,
-        icon: "success",
-        width: "350px"
+        title: "Estas seguro que deseas eliminar el servicio?",
+        showCancelButton: true,
+        confirmButtonText: "Confirmar",
+      }).then((result) => {
+        /* Read more about isConfirmed, isDenied below */
+        if (result.isConfirmed) {
+          Swal.fire("Servicio eliminado!", "", "success");
+        } 
       });
+      
     }
 
     const contadorCarro=()=>{
